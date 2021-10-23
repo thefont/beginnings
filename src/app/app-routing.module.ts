733a23e8b0199ff9d38951d15260788b1772d492
Eric Fontenault <eric@Erics-MBP.priorityonetv.com>
2021-10-23 03:30:10 -0500

    trying to get icons to work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { IconsComponent } from './icons/icons.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'icons',
+    component: IconsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
