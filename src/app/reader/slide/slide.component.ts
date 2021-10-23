9ab4c3c2eece71938ea4d4ebf77cbb76e5ab01cc
Chris Langham <chrislangham@rightnow.org>
2021-10-23 15:42:28 -0500

    fixing slide

--- src/app/reader/slide/slide.component.ts
+++ src/app/reader/slide/slide.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import { Observable } from 'rxjs';
+import {Observable, of} from 'rxjs';
 import { BibleService, PassageResponse } from 'src/app/providers/bible.service';
 import {SlideType} from '../../models/slide.enum';
+import {map, shareReplay, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-slide',
@@ -11,21 +12,20 @@ import {SlideType} from '../../models/slide.enum';
 })
 export class SlideComponent implements OnInit {
   passage$: Observable<PassageResponse>;
+  title$: Observable<string>;
+  SlideType = SlideType;
 
   constructor(private readonly route: ActivatedRoute, private readonly bibleService: BibleService) {
   }
 
   ngOnInit(): void {
-    this.passage$ = this.bibleService.getPassage("MAT.1.15-MAT.1.18", false);
+    this.passage$ = this.bibleService.getPassage('MAT.1.15-MAT.1.18', false).pipe(shareReplay());
+    this.title$ = this.passage$.pipe(map(x => this.route.snapshot.params.stepNumber + ': ' + 'Creation to Christ')); //todo do better
     console.log(this.route.snapshot.params.stepNumber);
     console.log(this.route.snapshot.params.slideNumber);
   }
 
-  getTitle() {
-    return this.route.snapshot.params.stepNumber + ': Who is Jesus'; //todo replace with step title
-  }
-
-  getSlideType() {
+  getSlideType(): SlideType {
     return SlideType.BibleVerse;
   }
 
