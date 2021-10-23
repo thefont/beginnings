f18bddff2db42da42f69903869dc8e084e1aa255
Eric Fontenault <eric@Erics-MBP.priorityonetv.com>
2021-10-23 11:52:41 -0500

    added service stubs and fixed compile issue

--- src/app/providers/bible.service.ts
+++ src/app/providers/bible.service.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 export interface BibleResponse {
   text: string; // update with actual api response
@@ -9,9 +11,15 @@ export interface BibleResponse {
   providedIn: 'root'
 })
 export class BibleService {
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getVerses(book: string, chapter: number, translationId: number, startVerse: number, endVerse?: number): Observable<BibleResponse> {
+    // return this.http.get('https://bible.api.whatever,').pipe(
+    //   map(response => {
+    //     // do any necessary mapping or model translation of the incoming data
+    //     return {} as BibleResponse;
+    //   })
+    // );
     return of({ text: 'In the beginning were cheetos' } as BibleResponse);
   }
 }
