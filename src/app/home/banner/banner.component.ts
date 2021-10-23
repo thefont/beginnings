42171687a6a89514a4fa1df6478149082aec8562
Andrew <Andrew@AndrewFrye-MBP.priorityonetv.com>
2021-10-23 15:55:54 -0500

    hides button on banners if necessary

--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -7,10 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent {
-  slides: { image: string; text?: string; descr?: string; }[] = [
-    { image: `assets/images/banners/0.jpeg`, text: `Creation to Christ`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`},
-    { image: `assets/images/banners/1.jpeg`, text: `Becoming Disciples`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`},
-    { image: `assets/images/banners/2.jpeg`, text: `Training Labourers`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`},
+  slides: { image: string; text?: string; descr?: string; action?: string; }[] = [
+    { image: `assets/images/banners/0.jpeg`, text: `Creation to Christ`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`, action: `Review`},
+    { image: `assets/images/banners/1.jpeg`, text: `Becoming Disciples`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`, action: `Review`},
+    { image: `assets/images/banners/2.jpeg`, text: `Training Labourers`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`, action: `Start`},
     { image: `assets/images/banners/3.jpeg`, text: `Stories of Hope`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`},
     { image: `assets/images/banners/4.jpeg`, text: `Seven Signs of John`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`},
     { image: `assets/images/banners/5.jpeg`, text: `For Such a Time`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`},
