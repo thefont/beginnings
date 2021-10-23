14f5e9cb5733a1565c55de15b6aef594f7378035
Andrew <Andrew@AndrewFrye-MBP.priorityonetv.com>
2021-10-23 15:05:20 -0500

    banners

--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -3,13 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-banner',
-  templateUrl: './banner.component.html'
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent {
-  slides: { image: string; text?: string }[] = [
-    { image: `assets/images/banners/0.jpeg`, text: `Creation to Christ`},
-    { image: `assets/images/banners/1.jpeg`, text: `Becoming Disciples`},
-    { image: `assets/images/banners/2.jpeg`, text: `Creation to Christ`}
+  slides: { image: string; text?: string; descr?: string; }[] = [
+    { image: `assets/images/banners/0.jpeg`, text: `Creation to Christ`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`},
+    { image: `assets/images/banners/1.jpeg`, text: `Becoming Disciples`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`},
+    { image: `assets/images/banners/2.jpeg`, text: `Training Labourers`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`},
+    { image: `assets/images/banners/2.jpeg`, text: `Stories of Hope`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`},
+    { image: `assets/images/banners/2.jpeg`, text: `Seven Signs of John`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`},
+    { image: `assets/images/banners/2.jpeg`, text: `For Such a Time`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`},
+    { image: `assets/images/banners/2.jpeg`, text: `Commands of Christ`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`},
+    { image: `assets/images/banners/2.jpeg`, text: `Discover Christmas`, descr: `Discover God's beautiful story of creation, and follow the threads all the way to Christ's arrival`}
   ];
   activeSlideIndex = 0;
 
