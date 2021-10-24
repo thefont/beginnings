2cc0d2e167aa5cb343f8c86444f714661665f568
Chris Langham <chrislangham@rightnow.org>
2021-10-23 20:20:34 -0500

    fixing import

--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -1,22 +1,22 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Step } from 'src/app/models/steps.model';
-import { StepService } from 'src/app/providers/step.service';
+import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Step} from 'src/app/models/steps.model';
+import {StepService} from 'src/app/providers/step.service';
 
 @Component({
-  // eslint-disable-next-line @angular-eslint/component-selector
-  selector: 'app-banner',
-  templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
+    // eslint-disable-next-line @angular-eslint/component-selector
+    selector: 'app-banner',
+    templateUrl: './banner.component.html',
+    styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent {
-  steps: Observable<Step[]>;
-  activeSlideIndex = 0;
+export class BannerComponent implements OnInit {
+    steps: Observable<Step[]>;
+    activeSlideIndex = 0;
 
-  constructor(private readonly stepService: StepService) {
-  }
+    constructor(private readonly stepService: StepService) {
+    }
 
-  ngOnInit(): void {
-    this.steps = this.stepService.getAllSteps();
-  }
+    ngOnInit(): void {
+        this.steps = this.stepService.getAllSteps();
+    }
 }
