de64f9cbcb32f7e855de431d7bad3ec033cb432c
Andrew <Andrew@AndrewFrye-MBP.priorityonetv.com>
2021-10-23 20:59:10 -0500

    some state management

--- src/app/home/banner/banner.component.ts
+++ src/app/home/banner/banner.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Observable } from 'rxjs';
 import { Step } from 'src/app/models/steps.model';
 import { StepService } from 'src/app/providers/step.service';
+import {StorageService} from '../../providers/storage.service';
 
 @Component({
   // eslint-disable-next-line @angular-eslint/component-selector
@@ -9,14 +10,20 @@ import { StepService } from 'src/app/providers/step.service';
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
-export class BannerComponent {
+export class BannerComponent implements OnInit {
   steps: Observable<Step[]>;
   activeSlideIndex = 0;
 
-  constructor(private readonly stepService: StepService) {
+  constructor(
+      private readonly stepService: StepService,
+      private readonly storageService: StorageService) {
   }
 
   ngOnInit(): void {
-    this.steps = this.stepService.getAllSteps();
+      const pathCompleted = this.storageService.get<boolean>('completedStep:8');
+      this.steps = this.stepService.getAllSteps(pathCompleted);
+      setTimeout(() => {
+          this.activeSlideIndex = this.storageService.get<number>('lastStep');
+      }, 500);
   }
 }
